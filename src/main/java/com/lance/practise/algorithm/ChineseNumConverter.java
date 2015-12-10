9eb3a6b44e183adec7d7ace5c63734e22a673d46
Shenglan Wang <lanlan.wsl@qq.com>
2015-12-10 19:49:02 +0800

    Init java project
    - add one algorithm: convert integer to Chinese number
    - add builder design pattern

---
+++ src/main/java/com/lance/practise/algorithm/ChineseNumConverter.java
@@ -0,0 +1,55 @@
+package com.lance.practise.algorithm;
+
+/**
+ * 将任意int转换成中文的字符串,如123转换为一百二十三
+ */
+public class ChineseNumConverter {
+
+    private static final String CHI_NEG = "负";
+
+    private static final String CHI_ZERO = "零";
+
+    private static final String[] CHI_DIGITS = { "", "一", "二", "三", "四", "五", "六", "七", "八", "九" };
+
+    private static final String[] CHI_UNITS = { "", "十", "百", "千" };
+
+    private static final String[] CHI_GROUPS = { "", "万", "亿" };
+
+    public static String toChinese(int num) {
+        if (num == 0) {
+            return CHI_ZERO;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("");
+        if (num < 0) {
+            sb.append(CHI_NEG);
+            num = -num;
+        }
+
+        String numStr = String.valueOf(num);
+        for (int pos = 0; pos < numStr.length() ; pos++) {
+            int unitIdx = pos % CHI_UNITS.length;
+            int groupIdx = pos / CHI_GROUPS.length;
+
+            int digitIdx = num % 10;
+            num /= 10;
+
+            if (unitIdx == 0) {
+                sb.append(CHI_GROUPS[groupIdx]);
+            }
+
+            if (digitIdx != 0) {
+                sb.append(CHI_UNITS[unitIdx]);
+                sb.append(CHI_DIGITS[digitIdx]);
+            }
+        }
+
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(toChinese(123456));
+        System.out.println(toChinese(1234567890));
+    }
+}
---
+++ src/main/java/com/lance/practise/pattern/Person.java
@@ -0,0 +1,74 @@
+package com.lance.practise.pattern;
+
+/**
+ * 设计模式: Builder模式
+ */
+public class Person {
+
+    private final String firstName;
+
+    private final String lastName;
+
+    private final String hobby;
+
+    private final int age;
+
+    private final boolean canSwim;
+
+    public static class PersonBuilder {
+
+        private final String firstName;
+
+        private final String lastName;
+
+        // Optional field
+        private String hobby = "sing";
+
+        private int age = 10;
+
+        private boolean canSwim = false;
+
+        public PersonBuilder(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public PersonBuilder setHobby(String hobby) {
+            this.hobby = hobby;
+            return this;
+        }
+
+        public PersonBuilder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public PersonBuilder setCanSwim(boolean canSwim) {
+            this.canSwim = canSwim;
+            return this;
+        }
+
+        public Person build() {
+            return new Person(this);
+        }
+    }
+
+    private Person(PersonBuilder builder) {
+        firstName = builder.firstName;
+        lastName = builder.lastName;
+        hobby = builder.hobby;
+        age = builder.age;
+        canSwim = builder.canSwim;
+    }
+
+    @Override
+    public String toString() {
+        return "BuilderPattern{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", hobby='" + hobby + '\'' +
+                ", age=" + age +
+                ", canSwim=" + canSwim +
+                '}';
+    }
+}
